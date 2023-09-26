c210661d80182b319dccacc996d0c4273ebb98e9
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2023-09-26 15:57:35 +0200

    add functioning timer to timerfunction.tsx

--- src/components/TimerFunction.tsx
+++ src/components/TimerFunction.tsx
@@ -1,14 +1,60 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+function Timer() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [intervalId, setIntervalId] = useState<number | null>(null);
 
-function TimerFunction() {
+  const startTimer = () => {
+    if (!isRunning) {
+      const newIntervalId = window.setInterval(updateTimer, 10);
+      setIntervalId(newIntervalId);
+      setIsRunning(true);
+    }
+  };
+
+  const stopTimer = () => {
+    if (isRunning && intervalId !== null) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+      setIsRunning(false);
+    }
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setElapsedTime(0);
+  };
+
+  const updateTimer = () => {
+    setElapsedTime((prevElapsedTime) => prevElapsedTime + 10);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalId !== null) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [intervalId]);
+
+  const formatTime = (time: number): string => {
+    const minutes: number = Math.floor(time / 60000);
+    const seconds: number = Math.floor((time % 60000) / 1000);
+    const centiseconds: number = Math.floor((time % 60000) / 10) % 100;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${centiseconds.toString().padStart(2, '0')}`;
+  };
+
   return (
     <div>
-        This is the timer function module
+      <h1>Timer Function Module</h1>
+      <div id="timer">{formatTime(elapsedTime)}</div>
+      <button onClick={isRunning ? stopTimer : startTimer}>
+        {isRunning ? 'Stop' : 'Start'}
+      </button>
+      <button onClick={resetTimer}>Reset</button>
     </div>
-    
   );
 }
 
-export default TimerFunction;
+export default Timer;
