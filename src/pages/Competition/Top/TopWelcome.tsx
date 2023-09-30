3f97b96cce337d0a8d35ea1fb8c79f2cfbdbe513
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2023-09-30 22:00:11 +0200

    add startscreen image

--- src/pages/Competition/Top/TopWelcome.tsx
+++ src/pages/Competition/Top/TopWelcome.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAppContext } from '../../../AppState'; // Import the context hook
+import image from '../../../media/images/homeimage.jpeg'
 
 function TopWelcome() {
   
@@ -10,6 +11,9 @@ function TopWelcome() {
       <div className='showjumping-title'>
         <h5>SHOW JUMPING TIMER </h5> 
      
+      </div>
+      <div className='title-image'>
+      <img src={image} alt="Show Jumping" className="resized-image" />
       </div>
     </div>
   );
