2d5476499dc193bea4aed0f674b29b84263cd38f
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2023-10-01 07:59:41 +0200

    fixes bug in winner component

--- src/pages/Competition/Top/TimerActive/Winner.tsx
+++ src/pages/Competition/Top/TimerActive/Winner.tsx
@@ -10,18 +10,18 @@ function Winner() {
   return (
     <div className='winners-container'>
       <div className='third-place-container winner-ranked-container'>
-        <div className='winner-ranking-number'>3</div>
-        <div>{rankedRiders[2].name}</div>
+        <div className='winner-ranking-number'>3RD</div>
+        <div>{rankedRiders[2]?.name && <div>{rankedRiders[2].name}</div>}</div>
         <div className='winner-icon'><StarOutlineIcon sx={{ fontSize: 20 }}/></div>
       </div>
       <div className='first-place-container winner-ranked-container'>
         <div className='winner-ranking-number'>WINNER</div>
-        <div>{rankedRiders[0].name}</div>
+        <div>{rankedRiders[0]?.name && <div>{rankedRiders[0].name}</div>}</div>
         <div className='winner-icon'><EmojiEventsIcon sx={{ fontSize: 40 }}/></div>
       </div>
       <div className='second-place-container winner-ranked-container'>
-        <div className='winner-ranking-number'>2</div>
-        <div>{rankedRiders[1].name}</div>
+        <div className='winner-ranking-number'>2ND</div>
+        <div>{rankedRiders[1]?.name && <div>{rankedRiders[1].name}</div>}</div>
         <div className='winner-icon'><StarOutlineIcon sx={{ fontSize: 20 }}/></div>
       </div>
     </div>
