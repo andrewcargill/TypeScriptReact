dd8385fa08d68062482ab18d49cbd8924c1a37cd
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2023-10-05 19:02:48 +0200

    tidy css code!

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,19 +1,15 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  // Customize your theme settings here
   palette: {
     primary: {
       main: '#43a314',
-      // main: '#007bff',
     },
     secondary: {
       main: '#647aa8',
-      // main: '#8b8d88',
     },
     success: {
       main: '#287d1b',
-      // main: '#8b8d88',
     },
   },
 });
